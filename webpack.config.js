614d4e098174953f5058b21d6b4c97c5d40c5fc2
SMartinSci <sm674@cornell.edu>
2019-03-07 09:59:28 -0800

    Add webpack file, webpackconfig object is entry point for app.

--- webpack.config.js
+++ webpack.config.js
@@ -17,3 +17,21 @@ const webpackConfig = {
         contentBase: DIR_DIST
     }
 }
+
+// setup babel-loader
+const babelConfig = {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: ['babel-loader']
+}
+webpackConfig.module.rules.push(babelConfig);
+
+// CSS setup
+const cssConfig = {
+    test: /\.css$/,
+    exclude: /node_modules/,
+    use: ['style-loader', 'css-loader']
+}
+webpackConfig.module.rules.push(cssConfig);
+
+module.exports = webpackConfig;

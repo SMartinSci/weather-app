ff084e4a4ca68379327673c1bb143c0978b00bc8
SMartinSci <sm674@cornell.edu>
2019-03-08 08:59:15 -0800

    Add react sparklines and several more components and files.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,15 +7,6 @@ class App extends Component {
         super(props);
         this.state = {};
     }
-    render() {
-        return (
-            <div>
-                <span><i className="fab fa-react"></i></span>
-                <span>Weather App</span>
-                <span><i className="wi wi-day-sunny"></i></span>
-            </div>
-        );
-    }
 
     render() {
         return (

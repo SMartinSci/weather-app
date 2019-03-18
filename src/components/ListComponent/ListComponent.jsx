d2bd5fe2f7be1776afcc464c97929a387f311c88
SMartinSci <sm674@cornell.edu>
2019-03-18 05:50:34 -0700

    Update list and today components.

--- src/components/ListComponent/ListComponent.jsx
+++ src/components/ListComponent/ListComponent.jsx
@@ -3,17 +3,17 @@ import './ListComponent.css';
 import { getIconClassName } from '../../utils/utils';
 
 const SingleListItem = (props) => {
-    const weatherId = 200;
+    const { day, weatherId, description, mainTemperature } = props.data;
     const iconClass = getIconClassName(weatherId);
 
     return (
         <div className="single-list-item">
             <div className="li-info-container">
                 <div className="li-day">
-                    {props.day}
+                    {day}
                 </div>
-                <div className="li-temp-text">24&#x00B0;</div>
-                <div className="li-desc">Sky is clear</div>
+                <div className="li-temp-text">{mainTemperature}&#x00B0;</div>
+                <div className="li-desc">{description}</div>
             </div>
             <div className="li-weather-icon">
                 <i className={`wi wi-owm-${weatherId} ${iconClass}`}></i>
@@ -23,14 +23,8 @@ const SingleListItem = (props) => {
 }
 
 class ListComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri']
-        };
-    }
     render() {
-        const items = this.state.data.map(day => (<SingleListItem key={day} day={day} />))
+        const items = this.props.data.map(singleDayData => (<SingleListItem key={singleDayData.day} data={singleDayData} />))
         return (
             <div className="list-container">
                 {items}
--- src/components/TodayComponent/TodayComponent.jsx
+++ src/components/TodayComponent/TodayComponent.jsx
@@ -5,27 +5,34 @@ import { getIconClassName } from '../../utils/utils';
 class TodayComponent extends Component {
 
     render() {
-        const weatherId = 300;
+        const {
+            day, date,
+            weatherId, description,
+            mainTemperature, minTemperature, maxTemperature,
+            pressure, humidity, windSpeed } = this.props.data;
+
+        const { unit } = this.props;
+
         const iconClass = getIconClassName(weatherId);
-        const windSpeedUnit = 'm/s'; // other unit is:miles/hr
+        const windSpeedUnit = (unit === 'C') ? 'm/s' : 'miles/hr';
 
         return (
             <div className="today-component-container">
                 <div className="date-container">
-                    <div>Friday</div>
-                    <div>May 04,2018</div>
+                    <div>{day}</div>
+                    <div>{date}</div>
                 </div>
 
                 <div className="icon-desc-container">
                     <div className="icon-conatainer">
                         <i className={`wi wi-owm-${weatherId} weather-icon ${iconClass}`} ></i>
                     </div>
-                    <div className="weather-desc">Heavy Rain</div>
+                    <div className="weather-desc">{description}</div>
                 </div>
 
                 <div className="temp-container">
                     <div className="temp-text">
-                        <span>24</span>
+                        <span>{mainTemperature}</span>
                         <i className="wi wi-degrees"></i>
                     </div>
                     <div className="high-low-container">
@@ -35,7 +42,7 @@ class TodayComponent extends Component {
                             </span>
                             <span>Max</span>
                             <span>
-                                <span>28&#x00B0;</span>
+                                <span>{maxTemperature}&#x00B0;</span>
                             </span>
                         </div>
                         <div className="high-low-item">
@@ -44,7 +51,7 @@ class TodayComponent extends Component {
                             </span>
                             <span>Min</span>
                             <span>
-                                <span>18&#x00B0;</span>
+                                <span>{minTemperature}&#x00B0;</span>
                             </span>
                         </div>
                     </div>
@@ -54,22 +61,20 @@ class TodayComponent extends Component {
                     <div className="extra-info-item">
                         <span><i className="wi wi-humidity"></i></span>
                         <span>Humidity</span>
-                        <span>34%</span>
+                        <span>{humidity}%</span>
                     </div>
                     <div className="extra-info-item">
                         <span><i className="wi wi-barometer"></i></span>
                         <span>Pressure</span>
-                        <span>34 hPa</span>
+                        <span>{pressure} hPa</span>
                     </div>
                     <div className="extra-info-item">
                         <span><i className="wi wi-strong-wind"></i></span>
                         <span>Wind Speed</span>
-                        <span>10 {windSpeedUnit}</span>
+                        <span>{windSpeed} {windSpeedUnit}</span>
                     </div>
                 </div>
             </div>
         );
     }
 }
-
-export default TodayComponent;
